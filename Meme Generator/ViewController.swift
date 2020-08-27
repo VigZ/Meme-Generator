4f683540aa272c5739f03d968d0ca794fe157e10
vigz <kyle.vigorito@gmail.com>
2020-08-26 20:08:26 -0400

    added image picker functionality, and some basic constraints

--- Meme Generator/ViewController.swift
+++ Meme Generator/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(selectPicture))
+       
     }
 
+    @IBAction func setTopText(_ sender: Any) {
+    }
+    
+    @IBAction func setBottomText(_ sender: Any) {
+    }
+    
+    @IBAction func share(_ sender: Any) {
+    }
+    
+    @objc func selectPicture(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        imageView.image = image
 
+        dismiss(animated: true)
+
+    }
+    
+    
+    
 }
 
